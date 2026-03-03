e192b66386079bcaebb94582ca5a898c1f97e9d2
Muhammed Sinan <sinanmohammed429@gmail.com>
2026-03-03 17:50:12 +0530

    Add app.py for basic Flask server

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify(status="healthy")
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
